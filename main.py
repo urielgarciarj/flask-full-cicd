f9bff1db265e52f69b1d0ef6c755604fd9e19212
uriel-garcia <uriel.garcia@bringitps.com>
2024-05-14 00:40:42 -0600

    main.py added
    Signed-off-by: uriel-garcia <uriel.garcia@bringitps.com>
    
    	new file:   main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return "Hello, World!!"
+
+@app.route('/users/<user_id>')
+def get_user(user_id):
+    user = {
+        "id": user_id,
+        "name": "Alice",
+        "email": "Alice@gmail.com"
+    }
+    query = request.args.get('query')
+    if query:
+        user['query'] = query
+    return jsonify(user), 200
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    user = {
+        "id": data['id'],
+        "name": data['name'],
+        "email": data['email']
+    }
+    return jsonify(user), 201
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
